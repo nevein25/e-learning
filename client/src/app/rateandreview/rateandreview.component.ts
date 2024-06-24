6fb5238f4d71a2bd7a61828e4bf594745619d2ec
Peter G.Gouda <73116913+peter8890@users.noreply.github.com>
2024-06-24 22:18:48 +0300

    RateAndReview

--- client/src/app/rateandreview/rateandreview.component.ts
+++ client/src/app/rateandreview/rateandreview.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-rateandreview',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+    BrowserModule 
+    
+  ],
   templateUrl: './rateandreview.component.html',
   styleUrl: './rateandreview.component.css'
 })
 export class RateandreviewComponent {
-
+  max = 10;
+  rate = 7;
 }
